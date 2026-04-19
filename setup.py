d1005a043cc48061e76efeb3cf313a2ba2c97e60
Bhaker <noobhaker9@gmail.com>
2026-04-19 18:56:33 +0530

    Simplify py2app config

--- setup.py
+++ setup.py
@@ -5,14 +5,10 @@ APP = ['gui.py']
 OPTIONS = {
     'app': {
         'name': 'Worker Activity Tracker',
-        'bundle_identifier': 'com.worker.app',
     },
-    'packages': ['requests', 'pillow', 'pystray', 'dotenv', 'mss'],
-    'excludes': ['test', 'tkinter'],
 }
 
 setup(
     app=APP,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
 )
