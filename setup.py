acaa620b9df5f185444d927a8baecfb753c2849c
Bhaker <noobhaker9@gmail.com>
2026-04-19 18:50:33 +0530

    a

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
 from setuptools import setup
-import platform
 
 APP = ['gui.py']
-DATA_FILES = []
-if platform.system() != 'Darwin':
-    DATA_FILES = [
-        ('F_icon.png', ['F_icon.png']),
-        ('F_icon.ico', ['F_icon.ico']),
-    ]
 
 OPTIONS = {
     'app': {
         'name': 'Worker Activity Tracker',
         'bundle_identifier': 'com.worker.app',
-        'icon_file': 'F_icon.ico',
-        'icon_size': (128, 128),
-        'minimal_python_version': '3.9',
-        'platform': 'MacOS',
     },
     'packages': ['requests', 'pillow', 'pystray', 'dotenv', 'mss'],
     'excludes': ['test', 'tkinter'],
-    'forceARC': True,
 }
 
 setup(
